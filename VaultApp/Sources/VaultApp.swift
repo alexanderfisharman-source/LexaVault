ffc36385b883f887b458cb283744feb33da18ecf
Yousif(Admin) <yousifthegoat2@gmail.com>
2026-03-15 21:43:52 +0400

    build: finalize xcodegen config with explicit rust linking

---
+++ VaultApp/Sources/VaultApp.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+
+@main
+struct VaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}import SwiftUI
+
+@main
+struct VaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
